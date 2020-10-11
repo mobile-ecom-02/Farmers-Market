a61de575ea93d1e3ca9aed734e7e96240c1ad488
Alex <alexeluro@gmail.com>
2020-10-11 20:42:23 +0100

    removed Room dependencies and annotations

--- app/src/main/java/com/ilatyphi95/farmersmarket/data/entities/User.kt
+++ app/src/main/java/com/ilatyphi95/farmersmarket/data/entities/User.kt
@@ -1,33 +1,23 @@
 package com.ilatyphi95.farmersmarket.data.entities
 
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 
-@Entity
 data class User (
 
-    @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(name = "id")
-    val id: Int,
+    val id: String = "",
 
-    @ColumnInfo(name = "first_name")
-    val firstName: String?,
+    val firstName: String,
 
-    @ColumnInfo(name = "last_name")
     val lastName: String,
 
-    @ColumnInfo(name = "email")
-    val email: String?,
+    val email: String,
 
-    @ColumnInfo(name = "phone")
-    val phone: String?,
+    val phone: String,
 
-    @ColumnInfo(name = "profile_pic_url")
-    val profilePicUrl: String?,
+    val profilePicUrl: String,
 
-    @ColumnInfo(name = "location")
-    val location: String?
+    val profileDisplayName: String,
+
+    val location: String
 
 
 )
