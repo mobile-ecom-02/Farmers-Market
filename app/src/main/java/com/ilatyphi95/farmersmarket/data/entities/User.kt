6679c8b62e2b5251591a921af58044bdcab8c27c
Ilatyphi95 <ilatyphi95@gmail.com>
2020-10-12 12:46:30 +0100

    Provide documentation to User entity

--- app/src/main/java/com/ilatyphi95/farmersmarket/data/entities/User.kt
+++ app/src/main/java/com/ilatyphi95/farmersmarket/data/entities/User.kt
@@ -1,23 +1,22 @@
 package com.ilatyphi95.farmersmarket.data.entities
 
-
-data class User (
-
-    val id: String = "",
-
+/**
+ * @param id unigue to each user
+ * @param firstName must be provided
+ * @param lastName must be provided
+ * @param email must be provided
+ * @param phone optional
+ * @param profilePicUrl optional
+ * @param profileDisplayName must be provided
+ * @param location must be provided
+ */
+data class User @JvmOverloads constructor(
+    var id: String = "",
     val firstName: String,
-
     val lastName: String,
-
     val email: String,
-
-    val phone: String,
-
-    val profilePicUrl: String,
-
+    val phone: String = "",
+    val profilePicUrl: String = "",
     val profileDisplayName: String,
-
     val location: String
-
-
 )
