d3822884a4d7973d836abe7bd4a69f1490135789
Ilatyphi95 <ilatyphi95@gmail.com>
2020-10-15 11:23:59 +0100

    Add universal recycler adapter

---
+++ app/src/main/java/com/ilatyphi95/farmersmarket/utils/BindingUtils.kt
@@ -0,0 +1,29 @@
+package com.ilatyphi95.farmersmarket.utils
+
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.ilatyphi95.farmersmarket.data.universaladapter.DataBindingRecyclerAdapter
+import com.ilatyphi95.farmersmarket.R
+import com.ilatyphi95.farmersmarket.data.universaladapter.RecyclerItem
+
+@BindingAdapter("app:availableText")
+fun TextView.availableText(qty: Int) {
+    text = this.context.getString(R.string.availableText, qty)
+}
+
+@BindingAdapter("items")
+fun setRecyclerViewItems(
+    recyclerView: RecyclerView,
+    items: List<RecyclerItem>?
+) {
+    var adapter = (recyclerView.adapter as? DataBindingRecyclerAdapter)
+    if (adapter == null) {
+        adapter = DataBindingRecyclerAdapter()
+        recyclerView.adapter = adapter
+    }
+
+    adapter.submitList(
+        items.orEmpty()
+    )
+}
