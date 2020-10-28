fe8119a3d48e9d665e3ee23c0b9aa70b0f6bbbed
Ilatyphi95 <ilatyphi95@gmail.com>
2020-10-28 08:22:45 +0100

    Revert commit 16bd55b

--- app/src/main/java/com/ilatyphi95/farmersmarket/LoginFragment.kt
+++ app/src/main/java/com/ilatyphi95/farmersmarket/LoginFragment.kt
@@ -10,14 +10,12 @@ import android.text.TextPaint
 import android.text.method.LinkMovementMethod
 import android.text.style.ClickableSpan
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.findNavController
 import androidx.transition.TransitionInflater
@@ -149,7 +147,8 @@ class LoginFragment : Fragment(){
                 Log.d(TAG, "logged in user ${it.result?.user?.uid}")
             }
             .addOnSuccessListener {
-                //code to go to home screen
+                findNavController().navigate(
+                    LoginFragmentDirections.actionLoginFragmentToHomeActivity())
             }
             .addOnFailureListener {
                 Log.d(TAG, "${it.message}")
--- app/src/main/java/com/ilatyphi95/farmersmarket/utils/ProductUtils.kt
+++ app/src/main/java/com/ilatyphi95/farmersmarket/utils/ProductUtils.kt
@@ -1,5 +1,6 @@
 package com.ilatyphi95.farmersmarket.utils
 
+import android.net.Uri
 import com.ilatyphi95.farmersmarket.R
 import com.ilatyphi95.farmersmarket.BR
 import com.ilatyphi95.farmersmarket.data.entities.CloseByProduct
@@ -61,3 +62,32 @@ fun CloseProductViewModel.toRecyclerItem() = RecyclerItem(
     layoutId = R.layout.close_product_item,
     variableId = BR.viewModel
 )
+
+sealed class PictureIconViewModel(val imageUrl: Uri?) {
+    lateinit var removeItemHandler: (imageUrl: Uri?) -> Unit
+    lateinit var addItemHandler: () -> Unit
+    open fun addItem() { addItemHandler() }
+    open fun removeItem() { removeItemHandler(imageUrl)}
+}
+
+fun PictureIconViewModel.toRecyclerItem() = RecyclerItem(
+    data = this,
+    layoutId = when(this) {
+        is AddIcon -> R.layout.add_icon
+        is AddedProductPicture -> R.layout.product_picture_layout },
+    variableId = BR.viewModel
+)
+
+class AddIcon : PictureIconViewModel(null) {
+    // do nothing
+    override fun removeItem() {
+    }
+}
+
+class AddedProductPicture(imageUri: Uri) : PictureIconViewModel(imageUri) {
+
+    // do nothing
+    override fun addItem() {
+    }
+}
+
